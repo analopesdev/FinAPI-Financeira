e9667b0f8a60de44e8f7098ea68bf177781631f3
Ana Lopes <analopesdemorais@outlook.com>
2022-10-30 17:34:31 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
+  return response.json(["curso1", "cruso2", "curso3"]);
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  return response.json(["curso1", "cruso2", "curso3", "curso4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  return response.json(["curso1", "cruso2", "curso3", "curso4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["curso1", "cruso2", "curso3", "curso4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["curso1", "cruso2", "curso3", "curso4"]);
+});
+
+app.listen(3333);
